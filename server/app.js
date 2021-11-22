c5e16fca70b125e89216452f4fd90012fc02f638
tgraha09 <tfire09@gmail.com>
2021-11-22 14:36:52 -0500

    Project 2

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -47,7 +47,7 @@ const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-//app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+// app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.disable('x-powered-by');
 app.use(cookieParser());
 
